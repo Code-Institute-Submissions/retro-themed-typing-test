8839e0e1aab2e0a8f91eb4c033c7aff8b56acb7d
bradleyplaydon <61969883+bradleyplaydon@users.noreply.github.com>
2021-04-01 00:33:10 +0100

    Added if statement to change text

--- assets/js/typing-game.js
+++ assets/js/typing-game.js
@@ -3,10 +3,21 @@ const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random'; //API I will call
 $(document).ready(function() {
   $("#start-game").click(function() {
     $("#welcome-section").css("display", "none");
+    playCounter();
   });
 });
 
 function playCounter() {
   var countToStart = setInterval(function() {
+    $("#startCountdown").text(function(i, text) {
+      if (parseInt(text) > 0) {
+        return parseInt(text) - 1;
+      } else {
+        clearTimeout(countToStart);
+        $("#typing-test").removeClass("d-none");
+        return "GO!";
+      }
+    });
+
   }, 1400);
 }
