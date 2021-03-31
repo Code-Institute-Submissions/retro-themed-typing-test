8bb959daed93a7325b4dcaa61dda2bcb53f13327
bradleyplaydon <61969883+bradleyplaydon@users.noreply.github.com>
2021-04-01 00:02:35 +0100

    Added click function for play button

---
+++ assets/js/typing-game.js
@@ -0,0 +1,7 @@
+const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random'; //API I will call to use the strings returned
+
+$(document).ready(function(){
+  $("#start-game").click(function(){
+    $("#welcome-section").css("display", "none");
+  });
+});
