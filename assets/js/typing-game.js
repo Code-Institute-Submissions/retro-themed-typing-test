ceec16a244e2925d5d786a7c9aab73b6c84d829a
bradleyplaydon <61969883+bradleyplaydon@users.noreply.github.com>
2021-04-01 00:25:42 +0100

    Init commit playCounter function

--- assets/js/typing-game.js
+++ assets/js/typing-game.js
@@ -1,7 +1,11 @@
 const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random'; //API I will call to use the strings returned
 
-$(document).ready(function(){
-  $("#start-game").click(function(){
+$(document).ready(function() {
+  $("#start-game").click(function() {
     $("#welcome-section").css("display", "none");
   });
 });
+
+function playCounter() {
+  
+}
