3909a797fcfa06270da54f29d84b0d1616d01944
bradleyplaydon <61969883+bradleyplaydon@users.noreply.github.com>
2021-04-04 01:51:23 +0100

    Added comments to JS

--- assets/js/typing-game.js
+++ assets/js/typing-game.js
@@ -2,12 +2,13 @@ const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random'; //API I will call
 
 $(document).ready(function() {
   $("#start-game").click(function() {
-    $("#welcome-section").css("display", "none");
-    $("#pre-countdown").removeClass("d-none");
+    $("#welcome-section").css("display", "none"); //Hides the welcome-section when the play button is clicked.
+    $("#pre-countdown").removeClass("d-none"); //Removes the display none class so we show the pre countdown timer when play button is clicked.
     playCounter();
   });
 });
 
+//Sets startCountdown text to 1 minus whatever number is already inputted into that field by parsing the text to an integer.
 function playCounter() {
   var countToStart = setInterval(function() {
     $("#startCountdown").text(function(i, text) {
