d1d4bbac5a05096e45bd908115491463acb1ae01
bradleyplaydon <61969883+bradleyplaydon@users.noreply.github.com>
2021-04-20 23:01:16 +0100

    Removed unnecessary function

--- assets/js/facebookSDK.js
+++ assets/js/facebookSDK.js
@@ -7,9 +7,4 @@ $.getScript('https://connect.facebook.net/en_US/sdk.js', function() {
     version: 'v2.7' // or v2.1, v2.2, v2.3, ...
   });
   $('#loginbutton,#feedbutton').removeAttr('disabled');
-  FB.getLoginStatus(updateStatusCallback);
 });
-
-function updateStatusCallback(){
-   console.log("status updated")
-}
