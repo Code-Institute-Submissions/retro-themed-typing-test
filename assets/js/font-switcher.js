01b2ce92265c5209275f7f99bfd2324450db4e18
bradleyplaydon <61969883+bradleyplaydon@users.noreply.github.com>
2021-04-20 23:09:17 +0100

    Added font switcher JS to set font

---
+++ assets/js/font-switcher.js
@@ -0,0 +1,18 @@
+$("#flexSwitchCheckDefault").change(function(){
+    if(this.checked == true){
+        setEasyToReadFont();
+    }
+    else {
+        setRetroFont();
+    }
+})
+
+var rootVars = document.querySelector(':root');
+
+function setEasyToReadFont() {
+  rootVars.style.setProperty('--retro-font', 'Helvetica');
+}
+
+function setRetroFont(){
+  rootVars.style.setProperty('--retro-font', 'Glass TTY VT220');
+}
