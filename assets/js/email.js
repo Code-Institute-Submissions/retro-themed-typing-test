fbb08c1d847d9e2e318e60418e2398e3657a05a7
bradleyplaydon <61969883+bradleyplaydon@users.noreply.github.com>
2021-04-21 23:03:50 +0100

    Added emailJS boilerplate JS

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,4 +1,24 @@
-// Variables required for emailJS
+// Variables for emailJS
 const serivceID = "service_veo9o7d";
 const templateID = "template_3093mur";
 const userID = "user_IZexurD0xkQkoR2UK3flk";
+
+// code fragment
+var dataT = {
+    service_id: serivceID,
+    template_id: templateID,
+    user_id: userID,
+    template_params: {
+        'username': 'example',
+    }
+};
+
+$.ajax('https://api.emailjs.com/api/v1.0/email/send', {
+    type: 'POST',
+    data: JSON.stringify(dataT),
+    contentType: 'application/json'
+}).done(function() {
+    alert('Your mail is sent!');
+}).fail(function(error) {
+    alert('Oops... ' + JSON.stringify(error));
+});
