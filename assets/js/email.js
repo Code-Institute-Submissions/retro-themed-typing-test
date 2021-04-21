b1860a744282df4e1e799536e8d41d23999e869a
bradleyplaydon <61969883+bradleyplaydon@users.noreply.github.com>
2021-04-22 00:02:07 +0100

    Beautify email.js

--- assets/js/email.js
+++ assets/js/email.js
@@ -3,29 +3,29 @@ const serivceID = "service_veo9o7d";
 const templateID = "template_3093mur";
 const userID = "user_IZexurD0xkQkoR2UK3flk";
 
-$(document).ready(function(){
-// code fragment
-var dataT = {
+$(document).ready(function() {
+  // code fragment
+  var dataT = {
     service_id: serivceID,
     template_id: templateID,
     user_id: userID,
     template_params: {
-        'from_name': "",
-        'from_email': "",
-        'contact_reason': "",
-        'message': ""
+      'from_name': "",
+      'from_email': "",
+      'contact_reason': "",
+      'message': ""
     }
-};
-$("#contactForm").submit(function(event) {
-$.ajax('https://api.emailjs.com/api/v1.0/email/send', {
-    type: 'POST',
-    data: JSON.stringify(dataT),
-    contentType: 'application/json'
-}).done(function() {
-    alert('Your mail is sent!');
-}).fail(function(error) {
-    alert('Oops... ' + JSON.stringify(error));
-});
-event.preventDefault();
-});
+  };
+  $("#contactForm").submit(function(event) {
+    $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
+      type: 'POST',
+      data: JSON.stringify(dataT),
+      contentType: 'application/json'
+    }).done(function() {
+      alert('Your mail is sent!');
+    }).fail(function(error) {
+      alert('Oops... ' + JSON.stringify(error));
+    });
+    event.preventDefault();
+  });
 });
